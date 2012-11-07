256697bedae0a764ae894baffa177fb8c692f24e
Viktor Ogeman <viktor.ogeman@gmail.com>
2012-11-07 21:30:54 +0100

    Using new library for watching drectory

--- watch/main.go
+++ watch/main.go
@@ -1,3 +1,6 @@
+
+// Package just modified from orginal package found at
+// http://code.google.com/p/rsc
 package main
 
 import (
@@ -7,43 +10,16 @@ import (
 	"os/exec"
 	"strings"
 	"sync"
-	"syscall"
-	"time"
+	//"syscall"
+	//"time"
 
+	"github.com/howeyc/fsnotify"
 	"code.google.com/p/goplan9/plan9/acme"
 )
 
 var args []string
 var win *acme.Win
 var needrun = make(chan bool, 1)
-
-var kq struct {
-	fd int
-	dir *os.File
-	m map[string]*os.File
-	name map[int]string
-}
-
-func kadd(fd int) {
-	kbuf := make([]syscall.Kevent_t, 1)
-	kbuf[0] = syscall.Kevent_t{
-		Ident: uint64(fd),
-		Filter: syscall.EVFILT_VNODE,
-		Flags: syscall.EV_ADD | syscall.EV_RECEIPT | syscall.EV_ONESHOT,
-		Fflags: syscall.NOTE_DELETE | syscall.NOTE_EXTEND | syscall.NOTE_WRITE,
-	}
-	n, err := syscall.Kevent(kq.fd, kbuf[:1], kbuf[:1], nil)
-	if err != nil {
-		log.Fatalf("kevent: %v", err)
-	}
-	ev := &kbuf[0]
-	if n != 1 || (ev.Flags&syscall.EV_ERROR) == 0 || int(ev.Ident) != int(fd) || int(ev.Filter) != syscall.EVFILT_VNODE {
-		log.Fatal("kqueue phase error")
-	}
-	if ev.Data != 0 {
-		log.Fatalf("kevent: kqueue error %s", syscall.Errno(ev.Data))
-	}
-}
 
 func main() {
 	flag.Parse()
@@ -61,74 +37,36 @@ func main() {
 	needrun <- true
 	go events()
 	go runner()
-	
-	kq.fd, err = syscall.Kqueue()
-	if err != nil {
-		log.Fatal(err)
-	}
-	kq.m = make(map[string]*os.File)
-	kq.name = make(map[int]string)
-
-	dir, err := os.Open(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-	kq.dir = dir
-	kadd(int(dir.Fd()))
-	readdir := true
-
-	for {
-		if readdir {
-		kq.dir.Seek(0, 0)
-		names, err := kq.dir.Readdirnames(-1)
-		if err != nil {
-			log.Fatalf("readdir: %v", err)
-		}
-		for _, name := range names {
-			if kq.m[name] != nil {
-				continue
-			}
-			f, err := os.Open(name)
-			if err != nil {
-				continue
-			}
-			kq.m[name] = f
-			fd := int(f.Fd())
-			kq.name[fd] = name
-			kadd(fd)
-		}
-		}
-
-		kbuf := make([]syscall.Kevent_t, 1)
-		var n int
-		for {
-			n, err = syscall.Kevent(kq.fd, nil, kbuf[:1], nil)
-			if err == syscall.EINTR {
-				continue
-			}
-			break
-		}
-		if err != nil {
-			log.Fatalf("kevent wait: %v", err)
-		}
-		ev := &kbuf[0]
-		if n != 1 || int(ev.Filter) != syscall.EVFILT_VNODE {
-			log.Fatal("kqueue phase error")
-		}
-
-		select {
-		case needrun <- true:
-		default:
-		}
-	
-		fd := int(ev.Ident) 
-		readdir = fd == int(kq.dir.Fd())
-		time.Sleep(100*time.Millisecond)
-		kadd(fd)
-	}
+	// Init new watcher 
+watcher, err := fsnotify.NewWatcher()
+if err != nil {
+    log.Fatal(err)  
 }
 
-func events() {
+go func() {
+    for {
+        select {
+        case ev := <-watcher.Event:
+            log.Println("event:", ev)
+	   needrun <- true
+        case err := <-watcher.Error:
+            log.Fatal("error:", err)
+        }
+    } 
+}()
+
+err = watcher.Watch(".") 
+if err != nil {
+    log.Fatal(err)
+} 
+
+// And now wait... ./watch go install
+select {}
+
+	log.Println("I am dead now...") 
+}
+
+func events() { 
 	for e := range win.EventChan() {
 		switch e.C2 {
 		case 'x', 'X': // execute
@@ -141,6 +79,8 @@ func events() {
 			}
 			if string(e.Text) == "Del" {
 				win.Ctl("delete")
+				// We should also stop watching
+				os.Exit(0)
 			}
 		}
 		win.WriteEvent(e)
